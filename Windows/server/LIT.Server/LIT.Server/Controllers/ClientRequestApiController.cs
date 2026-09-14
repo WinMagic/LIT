01792c82039be6023512e56218a1f02ef9f1823d
Sergei Nikitin <sergei.nikitin@winmagic.com>
2026-09-14 12:03:37 -0400

    License update

--- Windows/server/LIT.Server/LIT.Server/Controllers/ClientRequestApiController.cs
+++ Windows/server/LIT.Server/LIT.Server/Controllers/ClientRequestApiController.cs
@@ -1,18 +1,16 @@
 ﻿/*
-* Copyright (C) 2026 WinMagic Inc.
-*
+* Copyright (c) 2026 WinMagic Corp.
 * This file is part of the WinMagic LIT reference project.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* Alternatively, this file may be used under the terms of the WinMagic Inc.
-* Commercial License, which can be found at https://winmagic.com/en/legal/commercial_license/
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
+* This software is dual-licensed:
+* 
+* 1. GNU Affero General Public License v3.0 (AGPL-3.0)
+* 2. Commercial license available from WinMagic Corp.
+* 
+* You may use this file under the terms of the AGPL-3.0 license
+* included in the LICENSE file in the root of this repository.
+* 
+* For commercial licensing options, OEM redistribution rights,
+* proprietary use, or support agreements, please contact WinMagic.
 */
 
 using LIT.ServerMVC.Data.Dtos;
--- Windows/server/LIT.Server/LIT.Server/Models/ErrorViewModel.cs
+++ Windows/server/LIT.Server/LIT.Server/Models/ErrorViewModel.cs
@@ -1,3 +1,17 @@
+/*
+* Copyright (c) 2026 WinMagic Corp.
+* This file is part of the WinMagic LIT reference project.
+* This software is dual-licensed:
+* 
+* 1. GNU Affero General Public License v3.0 (AGPL-3.0)
+* 2. Commercial license available from WinMagic Corp.
+* 
+* You may use this file under the terms of the AGPL-3.0 license
+* included in the LICENSE file in the root of this repository.
+* 
+* For commercial licensing options, OEM redistribution rights,
+* proprietary use, or support agreements, please contact WinMagic.
+*/
 namespace LIT.ServerMVC.Models
 {
     public class ErrorViewModel
